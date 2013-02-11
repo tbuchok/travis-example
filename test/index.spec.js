05afdd6c60c242d4e2aa2f43aafcea9873419f83
Tom Buchok <git@buchok.com>
2013-02-10 21:26:38 -0500

    updates to use object.keys for browser testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,6 +5,7 @@ test('out travis and tap', function(t) {
   t.ok(result, 'The result was great!');
   var psuedo = null;
   t.notOk(psuedo, 'The result was expected to be `null`!');
-  t.equal(1, 1, 'one equals one');
+  var obj = { a : 'a', b : 'b', c : 'c' };
+  t.equal(Object.keys(obj).length, 3, 'one equals one');
   t.end();
 });
